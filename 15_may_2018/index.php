f36a2abca665e11b92c72cb49882c89085495b4b
Patryk Piwko <patrykpiwko123412@gmail.com>
2018-05-15 22:51:13 +0200

    check method from children class

--- 15_may_2018/index.php
+++ 15_may_2018/index.php
@@ -1,6 +1,18 @@
 <?php
 
 use Shop\Book;
+use Shop\Product;
+use Shop\CD;
+
 require_once __DIR__ . '/autoload.php';
 
-var_dump(get_class_methods(Book::class));
+//var_dump(get_class_methods(Book::class));
+
+if(new Book() instanceof Product){
+    echo 'Book class have instance in Product class';
+}
+
+
+if(! new Book() instanceof CD){
+    echo "<br>Book class doesn't have instance in CD class";
+}
